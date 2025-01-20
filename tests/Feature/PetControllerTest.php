182220c6b8541abb74b234623758463a7715460d
ccwrc <ccwrcltd@gmail.com>
2025-01-20 21:55:27 +0100

    PetControllerTest added

---
+++ tests/Feature/PetControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class PetControllerTest extends TestCase
+{
+
+    protected string $apiUrl;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->apiUrl = env('PETSTORE_API_URL', 'https://example.com');
+    }
+
+    public function test_create_displays_create_view()
+    {
+        $response = $this->get(route('pets.create'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pets.create');
+    }
+}
